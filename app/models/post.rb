8938f8cfeb7aed60c92f0e2118ba9b2601e1cfe3
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-03-14 13:51:52 -0700

    commit tree df54241ec33337a955f0323c87e3fa64e8e0baa5

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,32 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :must_include_bait_in_title
+
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+      ]
+      
+      bait = ["Won't Believe", "Secret", "Top", "Guess"]
+      # def must_include_bait_in_title
+    #     byebug
+    #     if bait.none? { |thing| thing.match title}
+    #         errors.add(:title, "sorry your title sucked")
+    #     end
+    # end
+
+
+    # below this is from the solution - but I dont' know why this works but a regular array doesn't
+      def must_include_bait_in_title
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+            byebug
+            errors.add(:title, "must be clickbait")
+          end
+        end
 end
